51962c8fd194c9d839cdc68d07895c3782e0bd46
notteelove0099 <146600508+notteelove0099@users.noreply.github.com>
2026-02-25 17:15:39 +0700

    Refactor firebase.js to mask sensitive data
    Updated firebase configuration to hide sensitive information.

--- firebase.js
+++ firebase.js
@@ -2,10 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzjzRdjIuOnb0U0uOFuxvyptNpu_x3usc",
-  databaseURL: "https://test-mode-f8aca-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "test-mode-f8aca",
+  apiKey: "......",
+  databaseURL: ".....",
+  projectId: ".....",
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getDatabase(app);
